760af5bd1364fb6fe894db52581dd26eed9d2e33
Rohith G <60336246+rohii52@users.noreply.github.com>
2025-01-28 14:47:43 +0000

    Add files via upload

---
+++ Basis Pursuit/Matlab Code/basis_pursuit.m
@@ -0,0 +1,36 @@
+clc;clear all;close all;
+n = 30;
+m = 10;
+A = randn(m,n);
+ 
+x = sprandn(n, 1, 0.1*n);
+b = A*x;
+ 
+xtrue = x;
+[X,Z,U] = basis_pursuit(A, b, 1.0, 1.0);
+function [x,z,u] = basis_pursuit(A, b, rho, alpha)
+    MAX_ITER = 1000;
+    [m n] = size(A);
+    x = zeros(n,1); z = zeros(n,1); u = zeros(n,1);
+ 
+    % precompute static variables for x-update (projection on to Ax=b)
+    AAt = A*A';
+    P = eye(n) - A' * (AAt \ A);
+    q = A' * (AAt \ b);
+ 
+    for k = 1:MAX_ITER
+        % x-update
+        x = P*(z - u) + q;
+ 
+        % z-update with relaxation
+        zold = z;
+        x_hat = alpha*x + (1 - alpha)*zold;
+        z = shrinkage(x_hat + u, 1/rho);
+ 
+        u = u + (x_hat - z);
+    end
+end
+ 
+function y = shrinkage(a, kappa)
+    y = max(0, a-kappa) - max(0, -a-kappa);
+end
